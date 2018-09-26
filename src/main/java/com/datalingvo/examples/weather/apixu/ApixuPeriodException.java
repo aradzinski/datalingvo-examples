8a8d103f30c1295303b25af26a66b5c82731514a
Aaron Radzinski <aradzinski@datalingvo.com>
2018-09-26 21:37:34 +0300

    2.1.0 release.

---
+++ src/main/java/com/datalingvo/examples/weather/apixu/ApixuPeriodException.java
@@ -0,0 +1,20 @@
+/*
+ * 2017 Copyright (C) DataLingvo, Inc. All Rights Reserved.
+ *       ___      _          __ _
+ *      /   \__ _| |_ __ _  / /(_)_ __   __ ___   _____
+ *     / /\ / _` | __/ _` |/ / | | '_ \ / _` \ \ / / _ \
+ *    / /_// (_| | || (_| / /__| | | | | (_| |\ V / (_) |
+ *   /___,' \__,_|\__\__,_\____/_|_| |_|\__, | \_/ \___/
+ *                                      |___/
+ */
+
+package com.datalingvo.examples.weather.apixu;
+
+/**
+ * APIXU date period limitation exception.
+ */
+public class ApixuPeriodException extends Exception {
+    public ApixuPeriodException(String message) {
+        super(message);
+    }
+}
